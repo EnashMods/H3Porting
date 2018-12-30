2e9a22d66083f339e56e4ca0b61031f83aefce9e
EnashMods <45928726+EnashMods@users.noreply.github.com>
2018-12-29 16:35:04 -0800

    included batch files

---
+++ Halo3Hud.bat
@@ -0,0 +1,24 @@
+ECHO Installing Halo 3 Hud
+
+
+color 0A
+
+if exist ..\..\..\maps\tags.dat (
+	GOTO Label0
+) else (
+	GOTO Label1
+)
+
+:Label0
+
+Type Halo3Hud.cmds | TagTool.exe ../../../maps/tags.dat
+
+ECHO Installation Complete ...
+
+CLEAR
+pause
+GOTO End
+
+:Label1
+ECHO WARNING Mod is not in the correct location.
+ECHO Install at: /mods/tagMods/Halo3Weapons
